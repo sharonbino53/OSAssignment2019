672c84fe8af4db750edf4f0597975fffe896e796
sharonbino53 <47522209+sharonbino53@users.noreply.github.com>
2019-04-06 16:24:44 +0530

    Update QuestionNo1.c

--- QuestionNo1.c
+++ QuestionNo1.c
@@ -4,5 +4,30 @@
 
 int main()
 {
+    int n = 0;
+    int d = 0;
     
-}
+    pid_t pid;
+    
+        do
+        {
+            printf("n\t\t\t Enter an integer greater than 0 : \t\t\t\n");
+            scanf("%d", &d);
+        }
+    while ( d <= 0 );
+    
+    pid = fork();
+    
+    if ( pid == 0 )
+    {
+        printf("The Child is Working Well.\n");
+        printf("%d\n",d);
+        while (d!=1)
+        {
+            if (d%2 ==0)
+            {
+                d = d/2;
+            }
+        }   
+    }
+}   
